551d4300bccabf1bc49087c8dc6577a3cf141dec
MiniMini-On <parkcy92@gmail.com>
2023-03-21 19:42:22 +0900

    :hammer: edit submission delete API
    edit submission delete API

--- homeworks/views.py
+++ homeworks/views.py
@@ -77,8 +77,8 @@ class SubmissionView(APIView):
                 storage.delete()
                 
                 return Response(data='storage deleted', status=status.HTTP_200_OK)
-        except:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        except Storage.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND, data='this storage dose not exist')
 
 """
 db값 추가
